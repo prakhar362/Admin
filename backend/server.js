c40ef8bd5e9d70b4bcf94d005df34ce1cb7f4f05
prakhar362 <prakharshri2005@gmail.com>
2025-01-20 19:41:51 +0530

    basic backend and db setup compelete

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+// Import dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public')); // Serve static files
+
+// Database connection
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('Connected to MongoDB Successfuly'))
+  .catch((err) => console.error('Database connection error:', err));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Welcome to the Main Admin Panel!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
